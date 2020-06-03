f41af908fff6b8c4cf0a563db87768cd14136195
Vitor Rios <vitor.neto@br.experian.com>
2020-06-03 00:15:17 -0300

    Creating routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,14 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req: Request, res: Response) => {
-  res.json(['Vitor', 'Rayza'])
-})
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333);
